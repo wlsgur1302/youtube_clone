a1574112bb2637020f46f4ad6cb2b576aa190f0d
Choi <wlsgur1302@naver.com>
2023-12-02 20:08:40 +0900

    페이지 만들기

---
+++ src/pages/Not.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Not() {
+  return (
+    <div>
+      Not
+    </div>
+  )
+}
+
+export default Not
---
+++ src/pages/Webd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Webd = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Webd
